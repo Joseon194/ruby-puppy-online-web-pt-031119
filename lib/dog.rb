93345c107c7292baeb74d254f21d47a10ba254e6
Dennis Chung <chungdennis6@gmail.com>
2019-04-08 02:11:36 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -13,7 +13,11 @@ class Dog
    @@all.clear
   end
   
+<<<<<<< HEAD
   def Dog.all
     @@all.each {|puppy| puts puppy.name}
    end
+=======
+  
+>>>>>>> 347d496b63b8cbf8e03041b7a48184c25684d47f
 end
